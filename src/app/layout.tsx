aa280eee08275dedba8e7ed922b05a1fddee7c5b
Raumil D <rdhandhu@asu.edu>
2024-09-11 22:07:11 -0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -50,11 +50,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <meta
-          name="keywords"
-          content="Big O notation, code analysis, algorithm analysis tool, ai algorithm analyzer, time complexity calculator, space complexity calculator, big o notation tool, analyze code performance, visualize algorithm complexity, code complexity checker, ai-driven algorithm insights, time and space complexity analysis, algorithm performance visualization, code benchmarking tool, measure algorithm efficiency, ai-powered code analysis, big o notation calculator, code complexity analysis tool, multi-language code analysis, ai code optimizer, predict time complexity, ai-based algorithm performance tool
-, algorithm, algorithm analysis, ai check, ai, ai for code,ai to code,my ai,ai bubble, ai searching, ai blog, ai blogspot, big o notation, time complexity, space complexity, code optimization, AI, AlgoMeter AI, algorithm visualization, time and space complexity, algometer, algometer ai, search ai, ai analysis, machine learning, leetcode, dsa, striver, neetcode, neet code, amazon, google, apple, faang, faamg"
-        />
+        <meta name="keywords" content="" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" type="image/svg+xml" href="/big-o-notation.svg" />
         <link
